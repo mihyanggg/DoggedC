1edbba5261dfb303cbe154e3cc020c464122bd25
mimi <mihyanggg@kakao.com>
2024-04-14 23:09:50 +0900

    s10-2.timecalc updated

--- basicExamples/s10-2.timecalc.c
+++ basicExamples/s10-2.timecalc.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
-#ifdef __linux__
-#elif defined(_WIN32)
-#endif
-
 int main(void)
 {
-    int user_s = 0, h = 0, m = 0, s = 0;
+    int user_s = 0/*, h = 0, m = 0, s = 0*/;
     
 #ifdef __linux__
     scanf("%d%*c", &user_s);
 #elif defined(_WIN32)
     scanf_s("%d%*c", &user_s);
 #endif
-    s = user_s % 60;
+    /*s = user_s % 60;
     m = user_s / 60;
     h = m / 60;
-    m %= 60;
+    m %= 60;*/
     
-    //printf("%02d:%02d:%02d\n", h, m, s);
-    printf("%d초는 %02d시간 %02d분 %02d초 입니다.\n", user_s, h, m, s);
+    //printf("%d초는 %02d시간 %02d분 %02d초 입니다.\n", user_s, h, m, s); // 02d:%02d:%02d
+    printf("%d초는 %02d시간 %02d분 %02d초 입니다.\n", user_s, user_s/3600, (user_s%3600)/60, user_s%60);
 
 }
